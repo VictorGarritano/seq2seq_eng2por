a8a109566cc06ff8bf2dc515b9f680396a3496a5
Victor Garritano <victortaichou@gmail.com>
2017-10-10 19:24:39 -0300

    Adding a more robust NN

--- translator.py
+++ translator.py
@@ -236,7 +236,7 @@ def readLangs(lang2, lang1='eng', reverse=False):
 # earlier).
 #
 
-MAX_LENGTH = 43
+MAX_LENGTH = 20
 
 eng_prefixes = (
     "i am ", "i m ",
@@ -795,16 +795,16 @@ def evaluateRandomly(encoder, decoder, n=10):
 #    encoder and decoder are initialized and run ``trainIters`` again.
 #
 
-hidden_size = 256
+hidden_size = 1024
 encoder1 = EncoderRNN(input_lang.n_words, hidden_size)
 attn_decoder1 = AttnDecoderRNN(hidden_size, output_lang.n_words,
-                               1, dropout_p=0.1)
+                               5, dropout_p=0.5)
 
 if use_cuda:
     encoder1 = encoder1.cuda()
     attn_decoder1 = attn_decoder1.cuda()
 
-trainIters(encoder1, attn_decoder1, 75000, print_every=1000)
+trainIters(encoder1, attn_decoder1, 75000, print_every=500)
 
 ######################################################################
 #
